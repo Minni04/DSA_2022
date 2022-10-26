1cf74e0e2a5ede1b48fa19e7eae135100ce36b02
Ginni13 <116739788+Ginni13@users.noreply.github.com>
2022-10-26 22:01:49 +0530

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,28 @@
+// Stack implementation in C++
+
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+#define MAX 10
+int size = 0;
+
+// Creating a stack
+struct stack {
+  int items[MAX];
+  int top;
+};
+typedef struct stack st;
+
+void createEmptyStack(st *s) {
+  s->top = -1;
+}
+
+// Check if the stack is full
+int isfull(st *s) {
+  if (s->top == MAX - 1)
+    return 1;
+  else
+    return 0;
+}
